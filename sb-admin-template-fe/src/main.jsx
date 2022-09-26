0b4954921b971c7ebfaf0c69f9d65557a0858b62
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-09-26 15:24:01 +0700

    try hash router

--- sb-admin-template-fe/src/main.jsx
+++ sb-admin-template-fe/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate, HashRouter} from "react-router-dom";
 
 import App from "./App";
 import UserList from "./pages/user/UserList.jsx";
@@ -24,50 +24,50 @@ import DetailProfileUserBooks from "./pages/user/DetailProfileUserBooks";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/">
-                    <Route index element={<Navigate to={"/home"} replace/>}/>
+            <HashRouter>
+                <Routes>
+                    <Route path="/">
+                        <Route index element={<Navigate to={"/home"} replace/>}/>
 
-                    <Route path={"/home"} element={<Home/>}/>
+                        <Route path={"/home"} element={<Home/>}/>
 
-                    <Route path={"register"} element={<RegisterForm/>}/>
-                    <Route path={"login"} element={<LoginForm/>}/>
+                        <Route path={"register"} element={<RegisterForm/>}/>
+                        <Route path={"login"} element={<LoginForm/>}/>
 
-                    <Route path={"/end"} element={<EndPage/>}/>
+                        <Route path={"/end"} element={<EndPage/>}/>
 
-                    <Route element={<App/>}>
-                        <Route path="users" element={<UserList/>}/>
-                        <Route path={"users/:username"} element={<DetailsProfile/>}/>
-                        <Route path={"users/:username/list-book"} element={<DetailProfileUserBooks/>}/>
-                        <Route
-                            path={"users/:username/:userId"}
-                            element={<ChangeProfile/>}
-                        />
+                        <Route element={<App/>}>
+                            <Route path="users" element={<UserList/>}/>
+                            <Route path={"users/:username"} element={<DetailsProfile/>}/>
+                            <Route path={"users/:username/list-book"} element={<DetailProfileUserBooks/>}/>
+                            <Route
+                                path={"users/:username/:userId"}
+                                element={<ChangeProfile/>}
+                            />
 
-                        <Route path={"roles"} element={<RoleList/>}/>
-                        <Route path={"roles/:roleId"} element={<ChangeRole/>}/>
-                        <Route path={"roles/add"} element={<AddRole/>}/>
+                            <Route path={"roles"} element={<RoleList/>}/>
+                            <Route path={"roles/:roleId"} element={<ChangeRole/>}/>
+                            <Route path={"roles/add"} element={<AddRole/>}/>
 
-                        <Route path={"/user/dashboard"} element={<UserDashboard/>}/>
+                            <Route path={"/user/dashboard"} element={<UserDashboard/>}/>
 
-                        <Route path={"/book/list"} element={<BookList/>}/>
+                            <Route path={"/book/list"} element={<BookList/>}/>
 
-                        <Route path={"/admin/dashboard"} element={<AdminDashboard/>}/>
+                            <Route path={"/admin/dashboard"} element={<AdminDashboard/>}/>
 
-                        <Route path="author" element={<AuthorList/>}/>
-                        <Route path="author/form" element={<AuthorForm/>}/>
-                        <Route path="author/form/:authorId" element={<AuthorForm/>}/>
+                            <Route path="author" element={<AuthorList/>}/>
+                            <Route path="author/form" element={<AuthorForm/>}/>
+                            <Route path="author/form/:authorId" element={<AuthorForm/>}/>
 
-                        <Route path="publisher" element={<PublisherList/>}/>
-                        <Route path="publisher/form" element={<PublisherForm/>}/>
-                        <Route
-                            path="publisher/form/:publisherId"
-                            element={<PublisherForm/>}
-                        />
+                            <Route path="publisher" element={<PublisherList/>}/>
+                            <Route path="publisher/form" element={<PublisherForm/>}/>
+                            <Route
+                                path="publisher/form/:publisherId"
+                                element={<PublisherForm/>}
+                            />
+                        </Route>
                     </Route>
-                </Route>
-            </Routes>
-        </BrowserRouter>
+                </Routes>
+            </HashRouter>
     </React.StrictMode>
 );
