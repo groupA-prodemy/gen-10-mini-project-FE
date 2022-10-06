17ef2b4100755d5363cf275fc1eda888c83b10e7
ridwan18001 <mini.project.kelompok1@gmail.com>
2022-10-06 22:53:45 +0700

    update userbook add

--- sb-admin-template-fe/src/pages/book/BookList2.jsx
+++ sb-admin-template-fe/src/pages/book/BookList2.jsx
@@ -43,9 +43,12 @@ export default function BookList2() {
                     <h6 className="m-0 font-weight-bold text-primary">
                         List Buku
                     </h6>
-                    <Link to="/book/form">
-                        <button className="btn btn-primary">Tambah Buku</button>
-                    </Link>
+                    {localStorage.getItem("role") != "Admin" ?
+                        <></> :
+                        <Link to="/book/form">
+                            <button className="btn btn-primary">Tambah Buku</button>
+                        </Link>
+                    }
                 </div>
 
                 <div className="card-body">
@@ -56,43 +59,51 @@ export default function BookList2() {
                             width="100%"
                             cellSpacing="0">
                             <thead>
-                            <tr>
-                                <th scope="col">No</th>
-                                <th>Judul Buku</th>
-                                <th>Kategori</th>
-                                <th>Tahun Terbit</th>
-                                <th>Author</th>
-                                <th>Publisher</th>
-                                <th>Status Buku</th>
-                                <th>Action</th>
-                            </tr>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th>Judul Buku</th>
+                                    <th>Kategori</th>
+                                    <th>Tahun Terbit</th>
+                                    <th>Author</th>
+                                    <th>Publisher</th>
+                                    <th>Status Buku</th>
+                                    {localStorage.getItem("role") != "Admin" ?
+                                        <></> :
+                                        <th>Action</th>
+                                    }
+
+                                </tr>
                             </thead>
                             <tbody>
-                            {books.map((books, index) => (
-                                <tr>
-                                    <td key={books.bookId} scope="row">{index + 1}</td>
-                                    <td>{books.bookTitle}</td>
-                                    <td>{books.categoryName}</td>
-                                    <td>{books.bookYear}</td>
-                                    <td>{books.authorName}</td>
-                                    <td>{books.publisherName}</td>
-                                    <td>{books.bookStatus === true ? "Tersedia" : "Dipinjam"}</td>
-                                    <td>
-                                        <Link to=
-                                                  {"/book/form/" + books.bookId}>
-                                            <button className="btn btn-primary"> Edit </button>
-                                        </Link>{" "}
-                                        <button
-                                            className="btn btn-danger"
-                                            onClick={() => deleteBook(books.bookId)}
+                                {books.map((books, index) => (
+                                    <tr>
+                                        <td key={books.bookId} scope="row">{index + 1}</td>
+                                        <td>{books.bookTitle}</td>
+                                        <td>{books.categoryName}</td>
+                                        <td>{books.bookYear}</td>
+                                        <td>{books.authorName}</td>
+                                        <td>{books.publisherName}</td>
+                                        <td>{books.bookStatus === true ? "Tersedia" : "Dipinjam"}</td>
+                                        {localStorage.getItem("role") != "Admin" ?
+                                            <></> :
+                                            <td>
+                                                <Link to=
+                                                    {"/book/form/" + books.bookId}>
+                                                    <button className="btn btn-primary"> Edit </button>
+                                                </Link>{" "}
+                                                <button
+                                                    className="btn btn-danger"
+                                                    onClick={() => deleteBook(books.bookId)}
 
-                                        >
-                                            {" "}
-                                            Hapus{" "}
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
+                                                >
+                                                    {" "}
+                                                    Hapus{" "}
+                                                </button>
+                                            </td>
+                                        }
+
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
--- sb-admin-template-fe/src/pages/userbook/UserbookForm.jsx
+++ sb-admin-template-fe/src/pages/userbook/UserbookForm.jsx
@@ -13,8 +13,8 @@ export default function UserBookForm() {
   const [userBooks, setUserBooks] = useState([]);
   const [userBookDetail, setUserBookDetail] = useState([]);
   const [formInput, setFormInput] = useState({
-    bookId:"",
-    userId:"",
+    bookId: "",
+    userId: "",
     startDate: "",
     dueDate: "",
     returnDate: ""
@@ -29,37 +29,89 @@ export default function UserBookForm() {
 
   async function getBooks() {
     const res = await axios.get(
-        "https://be-psm-mini-library-system.herokuapp.com/book/books"
+      "https://be-psm-mini-library-system.herokuapp.com/book/books"
     );
     setBooks(res.data);
   }
 
   async function getUsers() {
     const res = await axios.get(
-        "https://be-psm-mini-library-system.herokuapp.com/users/list-user"
+      "https://be-psm-mini-library-system.herokuapp.com/users/list-user"
     );
     setUsers(res.data);
   }
 
   async function getUserBooks() {
     const res = await fetch("https://be-psm-mini-library-system.herokuapp.com/userbook/list-userbook",
-        { method: "GET" })
+      { method: "GET" })
     const data = await res.json();
     setUserBooks(data);
   }
 
   async function getUserBookDetail() {
-    const res = await fetch("https://be-psm-mini-library-system.herokuapp.com/userbook/"+params.userbookId,
-        { method: "GET" })
+    const res = await fetch("https://be-psm-mini-library-system.herokuapp.com/userbook/" + params.userbookId,
+      { method: "GET" })
     const data = await res.json();
     setUserBookDetail(data);
   }
 
+  function userWhoCanBorrow() {
+    let arr = []
+    for (let x = 0; x < users.length; x++) {
+      for (let y = 0; y < userBooks.length; y++) {
+        if (users[x].username == userBooks[y].userName) {
+          if (userBooks[y].returnDate == null) {
+            arr.push(userBooks[y].userName)
+          }
+        }
+      }
+    }
+    return arr
+  }
+  localStorage.setItem("usname", userWhoCanBorrow())
+  const dataUsBook = localStorage.getItem("usname").split(",")
+  // const setArrB = new Set(dataUsBook)
+  console.log(setArrB)
+
+  function setCounter() {
+    let counter = 0
+    for (let x = 0; x<dataUsBook.length; x++) {
+      for (let y = 0; y<dataUsBook.length; y++) {
+        if (dataUsBook[x] == dataUsBook[y]) {
+          
+        }
+      }
+    }
+
+  }
+
+  // function usBookCounter() {
+  //   let counter = 0
+    
+  //   console.log(setArrB)
+  //   // let arrB
+  //   for (let x = 0; x < setArrB.size; x++) {
+  //     let arrB = dataUsBook.filter(n => n === userBooks[x].userName)
+  //     // console.log(arrB)
+  //     if (arrB.length < 3) {
+  //       counter = 0
+  //     } else {
+  //       counter = counter + arrB
+  //     }
+  //     // for (let y = 0; y < dataUsBook.length; y++) {
+  //     //   if (dataUsBook[y] == users[x].username) {
+
+  //     //   }
+  //     // }
+  //   }
+  //   return counter
+  // }
+  // // console.log(usBookCounter())
 
   async function getFormInput() {
     const res = await axios.get(
-        "https://be-psm-mini-library-system.herokuapp.com/userbook/" +
-        params.userbookId
+      "https://be-psm-mini-library-system.herokuapp.com/userbook/" +
+      params.userbookId
     );
     console.log(res.data)
     setFormInput(res.data.data);
@@ -69,24 +121,24 @@ export default function UserBookForm() {
     event.preventDefault();
 
     const payload = {
-      bookId:formInput.bookId,
-      userId:formInput.userId,
-      startDate:formInput.startDate.toString(),
-      dueDate:formInput.dueDate.toString(),
-      returnDate:formInput.returnDate.toString()
+      bookId: formInput.bookId,
+      userId: formInput.userId,
+      startDate: formInput.startDate.toString(),
+      dueDate: formInput.dueDate.toString(),
+      returnDate: formInput.returnDate.toString()
     }
 
     if (isEditting) {
       await axios.put(
-          "https://be-psm-mini-library-system.herokuapp.com/userbook/update-userbook/" +
-          params.userbookId,
-          payload
+        "https://be-psm-mini-library-system.herokuapp.com/userbook/update-userbook/" +
+        params.userbookId,
+        payload
 
       );
     } else {
       await axios.post(
-          "https://be-psm-mini-library-system.herokuapp.com/userbook/add-userbook",
-          payload
+        "https://be-psm-mini-library-system.herokuapp.com/userbook/add-userbook",
+        payload
       );
     }
     navigate("/userbook/list");
@@ -118,18 +170,18 @@ export default function UserBookForm() {
             <div className="mb-3">
               <label className="form-label">Judul Buku</label>
               <select
-                  className="form-control"
-                  value={formInput.bookId}
-                  required
-                  onChange={(event) => handleInput(event, "bookId")}
+                className="form-control"
+                value={formInput.bookId}
+                required
+                onChange={(event) => handleInput(event, "bookId")}
               >
                 <option value={""} disabled></option>
                 {books.map(book =>
-                    book.bookStatus === true?
-                        <option
-                            value={book.bookId}>
-                          {book.bookTitle}
-                        </option>:<></>
+                  book.bookStatus === true ?
+                    <option
+                      value={book.bookId}>
+                      {book.bookTitle}
+                    </option> : <></>
                 )}
               </select>
             </div>
@@ -137,50 +189,67 @@ export default function UserBookForm() {
             <div className="mb-3">
               <label className="form-label">username Pengguna</label>
               <select
-                  className="form-control"
-                  value={formInput.userId}
-                  required
-                  onChange={(event) => handleInput(event, "userId")}
+                className="form-control"
+                value={formInput.userId}
+                required
+                onChange={(event) => handleInput(event, "userId")}
               >
-                <option value={""} disabled></option>
-                {users.map(user =>
-                    <option
+                {isEditting ?
+                  <>
+                    <option value={""} disabled></option>
+                    {users.map(user =>
+                      <option
                         value={user.userId}>
-                      {user.username}
-                    </option>
-                )}
+                        {user.username}
+                      </option>
+                    )}
+                  </>
+                  :
+                    <>
+                    <option value={""} disabled></option>
+                    {users.map(user =>
+                      setArrB.has(user.username) ?
+                      <></>:
+                      <option
+                          value={user.userId}>
+                          {user.username}
+                        </option> 
+                    )}
+                  </>
+
+                }
               </select>
             </div>
 
             <div className="mb-3">
               <label className="form-label">Tanggal Peminjaman</label>
               <input
-                  className="form-control"
-                  type="date"
-                  required
-                  value={formInput.startDate}
-                  onChange={(event) => handleInput(event, "startDate")}
+                className="form-control"
+                type="date"
+                required
+                value={formInput.startDate}
+                onChange={(event) => handleInput(event, "startDate")}
               />
             </div>
 
             <div className="mb-3">
               <label className="form-label">Batas Peminjaman</label>
               <input
-                  className="form-control"
-                  type="date"
-                  required
-                  value={formInput.dueDate}
-                  onChange={(event) => handleInput(event, "dueDate")}
+                className="form-control"
+                type="date"
+                required
+                value={formInput.dueDate}
+                onChange={(event) => handleInput(event, "dueDate")}
               />
             </div>
 
             <div className="mb-3">
               <label className="form-label">Tanggal Pengembalian</label>
               <input
-                  className="form-control"
-                  type="date"
-                  value={formInput.returnDate}
-                  onChange={(event) => handleInput(event, "returnDate")}
+                className="form-control"
+                type="date"
+                value={formInput.returnDate}
+                onChange={(event) => handleInput(event, "returnDate")}
               />
             </div>
             <div>
