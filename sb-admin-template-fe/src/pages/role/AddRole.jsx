a528c304ef6e698d834518842f5faf3ebb2d41a2
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-10-09 01:24:55 +0700

    revise role FORM ADD for handling alert and set to lower case

--- sb-admin-template-fe/src/pages/role/AddRole.jsx
+++ sb-admin-template-fe/src/pages/role/AddRole.jsx
@@ -1,12 +1,32 @@
 import {Link, useNavigate} from "react-router-dom";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
+let responses = []
 export default function AddRole() {
-    let responses = []
+    let statusCheckerName = true
     const navigate = useNavigate()
+    const [roles, setRoles] = useState([])
     const [formInput, setFormInput] = useState({
         roleName: ""
     })
+
+    async function getUsers() {
+        const res = await fetch("https://be-psm-mini-library-system.herokuapp.com/role/list-role",
+            {method: "GET"})
+        const data = await res.json();
+        setRoles(data.sort((a,b)=>a.roleId-b.roleId));
+    }
+
+    function roleNameChecker(){
+        for(let role of roles){
+            if(role.roleName.toLowerCase()===formInput.roleName.toLowerCase()){
+                alert("Failed to save data, data was exists")
+                statusCheckerName = false
+            }
+        }
+    }
+
+
 
     function handleInput(event, inputName) {
         const copyFormInput = {...formInput}
@@ -20,28 +40,38 @@ export default function AddRole() {
             ...formInput,
             roleId: parseInt(formInput.roleId)
         })
-        const targetUrl = "https://be-psm-mini-library-system.herokuapp.com/role/save-role"
-        const method = "POST"
-        await fetch(targetUrl, {
-            method: method,
-            body: payload,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then((re) => re.json()).then((d) => responses.push(d))
-        if (responses[responses.length - 1].status.toString() === "true") {
-            alert
-            (
-                responses[responses.length - 1].message.toString()
-                + "\n" + "role Id: " + responses[responses.length - 1].data.roleId.toString()
-                + "\n" + "role Name: " + responses[responses.length - 1].data.roleName.toString()
-            )
-            navigate('/roles')
-        } else {
-            if (formInput.roleName !== "") {
-                alert(responses[responses.length - 1].message.toString())
+
+        roleNameChecker()
+        if(statusCheckerName === false ){
+            statusCheckerName = true
+        }else {
+            const targetUrl = "https://be-psm-mini-library-system.herokuapp.com/role/save-role"
+            const method = "POST"
+            const res = await fetch(targetUrl, {
+                method: method,
+                body: payload,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const resp = await res.json()
+            responses.push(resp)
+            if (responses[responses.length - 1].status === true) {
+                alert
+                (
+                    responses[responses.length - 1].message
+                    + "\n" + "role Id: " + responses[responses.length - 1].data.roleId.toString()
+                    + "\n" + "role Name: " + responses[responses.length - 1].data.roleName.toString()
+                )
+                statusCheckerName = true
+                navigate('/roles')
             } else {
-                alert("Form must be filled fully")
+                statusCheckerName = true
+                if (formInput.roleName !== "") {
+                    alert(responses[responses.length - 1].message)
+                } else {
+                    alert("Form must be filled fully")
+                }
             }
         }
     }
@@ -50,6 +80,10 @@ export default function AddRole() {
         event.preventDefault()
         history.go(-1)
     }
+
+    useEffect(()=>{
+        getUsers()
+    },[])
 
     return <>
         <div className="card shadow mb-4">
