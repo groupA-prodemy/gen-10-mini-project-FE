53bc7222e8d67ecab1ff7227e7992fc19f768bca
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-10-09 05:37:59 +0700

    revise category list

--- sb-admin-template-fe/src/pages/category/CategoryList.jsx
+++ sb-admin-template-fe/src/pages/category/CategoryList.jsx
@@ -1,91 +1,91 @@
 import React from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 export default function CategoryList() {
-  const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState([]);
 
-  async function getCategoryList() {
-    try {
-      const response = await axios.get(
-          "https://be-psm-mini-library-system.herokuapp.com/category/list"
-      );
-      setCategories(response.data.sort((a,b)=>a.categoryId-b.categoryId));
-    } catch (err) {
-      alert("Terjadi Masalah");
+    async function getCategoryList() {
+        try {
+            const response = await axios.get(
+                "https://be-psm-mini-library-system.herokuapp.com/category/list"
+            );
+            setCategories(response.data.sort((a, b) => a.categoryId - b.categoryId));
+        } catch (err) {
+            alert("Terjadi Masalah");
+        }
     }
-  }
 
-  function deleteCategory(id) {
-    axios
-        .delete(
-            "https://be-psm-mini-library-system.herokuapp.com/category/delete/" + id
-        )
-        .then(() => {
-          getCategoryList();
-        })
-        .catch((err) => {
-          console.log(err);
-          alert("error");
-        });
-  }
+    function deleteCategory(id) {
+        axios
+            .delete(
+                "https://be-psm-mini-library-system.herokuapp.com/category/delete/" + id
+            )
+            .then(() => {
+                getCategoryList();
+            })
+            .catch((err) => {
+                console.log(err);
+                alert("error");
+            });
+    }
 
-  useEffect(() => {
-    getCategoryList();
-  }, []);
+    useEffect(() => {
+        getCategoryList();
+    }, []);
 
-  return (
-      <>
-        <div className="card shadow mb-4">
-          <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 className="m-0 font-weight-bold text-primary">Daftar Kategori</h6>
-            <Link to="/category/form">
-              <button className="btn btn-primary"> Tambah Kategori </button>
-            </Link>
-          </div>
+    return (
+        <>
+            <div className="card shadow mb-4">
+                <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 className="m-0 font-weight-bold text-primary">Daftar Kategori</h6>
+                    <Link to="/category/form">
+                        <button className="btn btn-primary"> Tambah Kategori</button>
+                    </Link>
+                </div>
 
-          <div className="card-body">
-            <div className="table-responsive">
-              <table
-                  className="table table-bordered"
-                  id="dataTable"
-                  width="100%"
-                  cellsSpacing="0"
-              >
-                <thead>
-                <tr>
-                  <th scope="col">No</th>
-                  <th>Kategori</th>
-                  <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                {categories.map((category, index) => (
-                    <tr>
-                      <td key={category.categoryId} scope="row">
-                        {index + 1}
-                      </td>
-                      <td>{category.categoryName}</td>
-                      <td>
-                        <Link to={"/category/form/" + category.categoryId}>
-                          <button className="btn btn-primary"> Edit </button>
-                        </Link>{" "}
-                        <button
-                            onClick={() => deleteCategory(category.categoryId)}
-                            className="btn btn-danger"
+                <div className="card-body">
+                    <div className="table-responsive">
+                        <table
+                            className="table table-bordered"
+                            id="dataTable"
+                            width="100%"
+                            cellSpacing="0"
                         >
-                          {" "}
-                          Hapus{" "}
-                        </button>
-                      </td>
-                    </tr>
-                ))}
-                </tbody>
-              </table>
+                            <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th>Kategori</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {categories.map((category, index) => (
+                                <tr key={category.categoryId}>
+                                    <td scope="row">
+                                        {index + 1}
+                                    </td>
+                                    <td>{category.categoryName}</td>
+                                    <td>
+                                        <Link to={"/category/form/" + category.categoryId}>
+                                            <button className="btn btn-primary"> Edit</button>
+                                        </Link>{" "}
+                                        <button
+                                            onClick={() => deleteCategory(category.categoryId)}
+                                            className="btn btn-danger"
+                                        >
+                                            {" "}
+                                            Hapus{" "}
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </>
-  );
+        </>
+    );
 }
