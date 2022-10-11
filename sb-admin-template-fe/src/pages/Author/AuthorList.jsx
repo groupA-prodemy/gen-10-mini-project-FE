a567572c85d02236ab1136be7cbe22f49464e6e5
Rosita120505 <107414498+Rosita120505@users.noreply.github.com>
2022-10-11 14:46:33 +0700

    tampilan author dan publisher dinamis

--- sb-admin-template-fe/src/pages/Author/AuthorList.jsx
+++ sb-admin-template-fe/src/pages/Author/AuthorList.jsx
@@ -70,11 +70,12 @@ export default function AuthorList() {
 
   return (
     <>
-      <div class="card shadow mb-4">
-        <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h6 class="m-0 font-weight-bold text-primary">Author List</h6>
-
-          <form className="d-none d-sm-inline-block form-inline navbar-search">
+      <div className="card shadow mb-4">
+        <div className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+          <ul className={"navbar-nav mr-auto"}>
+            <h6 className="m-0 font-weight-bold text-primary">Author List</h6>
+          </ul>
+          <form className="d-none d-sm-inline-block form-inline mr-md-3 ml-md-3 my-2 my-md-0 mw-100 navbar-search">
             <div className="input-group">
               <input
                 type="text"
@@ -93,8 +94,58 @@ export default function AuthorList() {
             </div>
           </form>
 
-          <Link to="/author/form">
-            <button className="btn btn-primary"> Add Author </button>
+          <ul className="navbar-nav md-center">
+            <div className="dropdown no-arrow d-sm-none">
+              <a
+                className="dropdown-toggle"
+                href="#"
+                id="searchDropdown"
+                author="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i className="fas fa-search fa-fw"></i>
+              </a>
+              <div
+                className="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                aria-labelledby="searchDropdown"
+              >
+                <form className="form-inline mr-auto w-100 navbar-search">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control bg-md-white-auth-end border-0 small"
+                      placeholder="find author"
+                      aria-label="Search"
+                      aria-describedby="basic-addon2"
+                      value={searchKeyword}
+                      onChange={(evt) => setSearchKeyword(evt.target.value)}
+                    />
+                    <div className="input-group-append">
+                      <button className="btn btn-primary" type="button">
+                        <i className="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </ul>
+
+          <ul className={"navbar-nav ml-auto"}>
+            <Link to={"/author/form"} className="dropdown no-arrow d-sm-none">
+              <button className="btn btn-primary">
+                <strong>+</strong>
+              </button>
+            </Link>
+          </ul>
+
+          <Link
+            to={"/author/form"}
+            className="d-none d-sm-inline-block form-inline mr-0 ml-md-3 my-2 my-md-0 mw-100"
+          >
+            <button className="btn btn-primary">Add Author</button>
           </Link>
         </div>
 
